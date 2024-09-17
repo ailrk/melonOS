6de1bd35920d9c061d304c8944f7a57c555993d3
Ailrk <jimmy123good@gmail.com>
2024-09-17 11:34:11 -0700

    add sleeplock

---
+++ kernel/sleeplock.c
@@ -0,0 +1,20 @@
+#include "sleeplock.h"
+#include "spinlock.h"
+#include "process/proc.h"
+
+
+SleepLock new_sleeplock(const char *name) {
+    SleepLock slk;
+    slk.lk = new_lock("sleeplk.spinlk");
+    slk.locked = false;
+    slk.name = name;
+    slk.pid = 0;
+    return slk;
+}
+
+
+void lock_sleep(SleepLock *slk) {
+    lock(&slk->lk);
+
+
+}
